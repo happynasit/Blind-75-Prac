ecde0f8c8bedd815a8e40a057225a91539783dc4
happynasit <105992603+happynasit@users.noreply.github.com>
2024-06-16 09:58:13 +0300

    Created Q1.py

---
+++ Q1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        create each pair and store it as a list
+        then for each pair we check their sum and whoever's sum is the target is the
+        two sum list
+        """
+
+  
