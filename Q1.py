80ada363b7d7bbda9b17491aae83ef8be7ad05f7
happynasit <105992603+happynasit@users.noreply.github.com>
2024-06-16 10:11:06 +0300

    Completed ARRAYS Easy: Two Sum

--- Q1.py
+++ Q1.py
@@ -5,5 +5,17 @@ class Solution:
         then for each pair we check their sum and whoever's sum is the target is the
         two sum list
         """
+        for i in range(len(nums)):
+            # Finding the difference
+            diff = target - nums[i]
 
-  
+            list_nums = nums
+            list_nums[i] = "_"
+
+            for j in range(len(list_nums)):
+                if list_nums[j] == diff:
+                    return [i, j]
+                
+            list_nums[i] = nums[i]
+
+        
