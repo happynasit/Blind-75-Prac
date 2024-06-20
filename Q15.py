430912c9bc6b98ac50066d256d583e0ba0c6117f
happynasit <105992603+happynasit@users.noreply.github.com>
2024-06-20 22:19:35 +0300

    Created ARRAYS Medium: 3Sum

---
+++ Q15.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums = nums.sort()
+        result = []
+        for i in range(len(nums) - 2):
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -= 1
+                else:
+                    triplet = [nums[i], nums[l], nums[r]]
+                    result.append(triplet)
+                    while l < r and nums[l] == triplet[1]:
+                        l += 1
+                    while l < r and nums[r] == triplet[2]:
+                        r -= 1
+        return result
