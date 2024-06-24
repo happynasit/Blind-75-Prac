ccf217f5a5bd285e1c5989e4778f9f5ca5e5413d
happynasit <105992603+happynasit@users.noreply.github.com>
2024-06-24 22:12:27 +0300

    Completed ARRAYS Medium: 3Sum

--- Q15.py
+++ Q15.py
@@ -3,19 +3,20 @@ class Solution:
         nums = nums.sort()
         result = []
         for i in range(len(nums) - 2):
-            l = i + 1
-            r = len(nums) - 1
-            while l < r:
-                total = nums[i] + nums[l] + nums[r]
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[left] + nums[right] + nums[i]
                 if total < 0:
-                    l += 1
+                    left +=1
                 elif total > 0:
-                    r -= 1
+                    right -=1
                 else:
-                    triplet = [nums[i], nums[l], nums[r]]
+                    triplet = [nums[left], nums[right], nums[i]]
                     result.append(triplet)
-                    while l < r and nums[l] == triplet[1]:
-                        l += 1
-                    while l < r and nums[r] == triplet[2]:
-                        r -= 1
+                    while left < right and nums[left] == triplet[0]:
+                        left += 1
+                    while left < right and nums[right] == triplet[1]:
+                        right -= 1
         return result
