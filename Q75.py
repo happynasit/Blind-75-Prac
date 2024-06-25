d41955b8030789fd7a8e37ea7f4a7b48b6d4410b
happynasit <105992603+happynasit@users.noreply.github.com>
2024-06-25 21:03:10 +0300

    Completed ARRAYS Medium: Sort Colors

--- Q75.py
+++ Q75.py
@@ -2,6 +2,7 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do NOT return anything, MODIFY nums in-place instead.
+        Same type of question asked in one of the interviews except it was real colours instead of integers 0, 1 and 2.
         """
         zero = nums.count(0)
         one = nums.count(1)
