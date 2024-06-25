5d321d4a978037b5f5a29429864756c8685d2b5a
happynasit <105992603+happynasit@users.noreply.github.com>
2024-06-25 20:58:56 +0300

    Completed ARRAYS Medium: Sort Colours

---
+++ Q75.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do NOT return anything, MODIFY nums in-place instead.
+        """
+        zero = nums.count(0)
+        one = nums.count(1)
+        two = nums.count(2)
+
+        print(zero)
+        print(one)
+        print(two)
+        
+        for i in range(0, zero):
+            nums[i] = 0
+        for j in range(zero, zero + one):
+            nums[j] = 1
+        for k in range(zero + one, len(nums)):
+            nums[k] = 2
+        
