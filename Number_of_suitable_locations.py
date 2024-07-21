99440f335308ca7c2e23bfcca2707f58c008cdc6
happynasit <105992603+happynasit@users.noreply.github.com>
2024-07-21 13:34:57 +0300

    Create Number_of_suitable_locations.py

---
+++ Number_of_suitable_locations.py
@@ -0,0 +1,11 @@
+"""
+Number of Suitable Locations
+🔥 FULLTIME
+Amazon has multiple delivery centers and delivery warehouses all over the world! The world is represented by a number line from -109 to 109. 
+There are n delivery centers, the ith one at location center[i]. A location x is called a suitable location for a warehouse if it is possible to bring 
+all the products to that point by traveling a distance of no more than d. At any one time, products can be brought from one delivery center and placed at 
+point x. Given the positions of n delivery centers, calculate the number of suitable locations in the world. That is, calculate the number of points x on the 
+number line (-109 ≤ x ≤ 109) where the travel distance required to bring all the products to that point is less than or equal to d.
+Note: The distance between point x and center[i] is |x - center[i]|, their absolute difference
+"""
+
