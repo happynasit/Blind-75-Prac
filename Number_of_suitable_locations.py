e6339e20bade698832a74af355b35f7feb76b277
happynasit <105992603+happynasit@users.noreply.github.com>
2024-07-21 13:35:29 +0300

    Update Number_of_suitable_locations.py

--- Number_of_suitable_locations.py
+++ Number_of_suitable_locations.py
@@ -7,5 +7,15 @@ all the products to that point by traveling a distance of no more than d. At any
 point x. Given the positions of n delivery centers, calculate the number of suitable locations in the world. That is, calculate the number of points x on the 
 number line (-109 ≤ x ≤ 109) where the travel distance required to bring all the products to that point is less than or equal to d.
 Note: The distance between point x and center[i] is |x - center[i]|, their absolute difference
+
+Input:  center = [2, 0, 3, -4], d = 22
+Output: 5 
+Explanation:
+There are 3 suitable locations i.e {-1, 0, 1, 2, 3}.
+1. Place a warehouse at x = -1, total distance traveled is 2 * |-1 - 2| + 2 * |-1 - 0| + 2 * |-1 - 3| + 2 * |-1 - (-4)| = 22 <= d.
+2. x = 0, total distance traveled is 2 * |0 - 2| + 2 * |0 - 0| + 2 * |0 - 3| + 2 * |0 - (-4)| = 18 <= d.
+3. x = 1, total distance traveled is 2 * |1 - 2| + 2 * |1 - 0| + 2 * |1 - 3| + 2 * |1 - (-4)| = 18 <= d.
+4. x = 2, total distance traveled is 2 * |2 - 2| + 2 * |2 - 0| + 2 * |2 - 3| + 2 * |2 - (-4)| = 18 <= d.
+5. x = 3, total distance traveled is 2 * |3 - 2| + 2 * |3 - 0| + 2 * |3 - 3| + 2 * |3 - (-4)| = 22 <= d.
 """
 
