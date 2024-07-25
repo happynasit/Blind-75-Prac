859d338d62feaf11a8b0f222deec17066909e505
happynasit <105992603+happynasit@users.noreply.github.com>
2024-07-25 15:32:28 +0300

    Update Q189.py

--- Q189.py
+++ Q189.py
@@ -3,9 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for _ in range(k):
-            # the value in the right
-            val = nums.pop()
-            print(val)
-            nums.insert(0, val)
-            print(nums)
+
+        # len_ = k % len(nums)
+        # for _ in range(len_):
+        #     # the value in the right
+        #     val = nums.pop()
+        #     print(val)
+        #     nums.insert(0, val)
+        #     print(nums)
+
+        if k == 0:
+            return
+
+        size = len(nums)
+
+        # this is in case some elements don't really need a rotation
+        k = k if k <= size else k % size
+        nums[k:], nums[:k] = nums[:size - k], nums[size - k:]
