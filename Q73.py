47253db9b1ba438592630a7ad7df80125a45b5b0
happynasit <105992603+happynasit@users.noreply.github.com>
2024-07-27 15:54:11 +0300

    Completed MATRIX Medium: Set Matrix Zeroes

--- Q73.py
+++ Q73.py
@@ -2,24 +2,44 @@ class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
-        """
-        positions = dict()
-        for row in range(len(matrix)):
-            for column in range(len(matrix[row])):
-                if matrix[row][column] == 0:
-                    # The entire row becomes 0
-                    #matrix[row] = [0 for i in range(len(matrix[row]))]
-                    if row in positions:
-                        positions[row].append(column)
-                    else:
-                        positions[row] = [column]
-        print(positions)
 
-        for row in positions.keys():
-            matrix[row] = [0 for i in range(len(matrix[row]))]
+        THE SECOND CODE RUNS FASTER, THAT IS FASTER THAN 83. something percent.
+        And its easy to understand also, its just by default assigning the arrays to False, and when the thing is 0, then assign it to true.
+        # """
+        # rows = []
+        # columns = []
+        # for row in range(len(matrix)):
+        #     for column in range(len(matrix[row])):
+        #         if matrix[row][column] == 0:
+        #             # The entire row becomes 0
+        #             rows.append(row)
+        #             columns.append(column)
 
-        for columns in positions.values():
-            for column in columns:
-                for r in range(len(matrix)):
-                    matrix[r][column] = 0
-        print(matrix)
+        # for row in rows:
+        #     matrix[row] = [0 for i in range(len(matrix[row]))]
+
+        # for column in columns:
+        #     for r in range(len(matrix)):
+        #         matrix[r][column] = 0
+
+        # print(matrix)
+        rLen = len(matrix)
+        cLen = len(matrix[0])
+        rows = [False] * rLen
+        cols = [False] * cLen
+        for r in range(rLen):
+            for c in range(cLen): # Traverse through matrix and save rows and cols that must be 0
+                if matrix[r][c] == 0:
+                    rows[r] = True
+                    cols[c] = True
+
+        print(rows)
+        print(cols)
+        for i, r in enumerate(rows): # iterate through rows that must be 0 and set to 0
+            if r:
+                for c in range(cLen):
+                    matrix[i][c] = 0
+        for j, c in enumerate(cols): # iterate through cols that must be 0 and set to 0
+            if c:
+                for r in range(rLen):
+                    matrix[r][j] = 0
