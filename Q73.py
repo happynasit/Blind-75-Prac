078527dd68aa909c378f797a6d5277507e9904b3
happynasit <105992603+happynasit@users.noreply.github.com>
2024-07-27 15:43:46 +0300

    Completed MATRIX Medium: Set Matrix Zeroes

--- Q73.py
+++ Q73.py
@@ -3,15 +3,23 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        positions = []
+        positions = dict()
         for row in range(len(matrix)):
             for column in range(len(matrix[row])):
                 if matrix[row][column] == 0:
                     # The entire row becomes 0
-                    positions.append(column)
-                    print(positions)
-                    matrix[row] = [0 for i in range(len(matrix[row]))]
-        for row in range(len(matrix)):
-            for position in positions:
-                matrix[row][position] = 0
+                    #matrix[row] = [0 for i in range(len(matrix[row]))]
+                    if row in positions:
+                        positions[row].append(column)
+                    else:
+                        positions[row] = [column]
+        print(positions)
+
+        for row in positions.keys():
+            matrix[row] = [0 for i in range(len(matrix[row]))]
+
+        for columns in positions.values():
+            for column in columns:
+                for r in range(len(matrix)):
+                    matrix[r][column] = 0
         print(matrix)
