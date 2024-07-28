e0d603b24c475a0b0618027a4361cd4782cd7c3e
happynasit <105992603+happynasit@users.noreply.github.com>
2024-07-28 17:01:59 +0300

    Completed ARRAYS Hard: Median of Two Sorted Arrays

---
+++ Q4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        l = []
+        l.extend(nums1)
+        l.extend(nums2)
+        l.sort()
+        if len(l) % 2 != 0:
+            index = (len(l) // 2)
+            return l[index]
+        else:
+            index = (len(l) // 2)
+            return (l[index] + l[index - 1]) / 2
