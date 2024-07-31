8f273e35b2fa71542e677f448612b6a94b4b5e92
happynasit <105992603+happynasit@users.noreply.github.com>
2024-07-31 16:14:58 +0300

    Completed DIVIDE AND CONQUER Easy: Majority Element

--- Q169.py
+++ Q169.py
@@ -1,7 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         new_lst = set(nums)
-        
         for value in new_lst:
             if nums.count(value) > (len(nums) // 2):
                 return value
