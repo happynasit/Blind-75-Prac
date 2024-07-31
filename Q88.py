60e873ed84c54a0781a34bbeba39845e8d2b313c
happynasit <105992603+happynasit@users.noreply.github.com>
2024-07-31 14:47:29 +0300

    Update Q88.py

--- Q88.py
+++ Q88.py
@@ -2,6 +2,8 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
+        SOLUTION:
+        Since the for loop takes O(n), and the sort takes O((m+n)log(m+n)), the time it takes would be O(m+n).
         """
         # this is for the nums 2 part, we will replace it from
         for i in range(n):
