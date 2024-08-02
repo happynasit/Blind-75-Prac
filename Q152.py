5f1183121018934e99619ebeff2076f8f8b53adf
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-02 16:57:45 +0300

    Completed ARRAYS Medium: Maximum Product Subarray

---
+++ Q152.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        # return max_sum
+        maxSum = float('-inf')
+        currentSum = 1
+        
+        for num in nums:
+            currentSum *= num
+            
+            if currentSum > maxSum:
+                maxSum = currentSum
+            
+            if currentSum < 0:
+                currentSum = 1
+        
+        return maxSum
