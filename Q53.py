9cea1ccda2476f64cd5916ab3f5fe753cf578272
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-02 16:36:41 +0300

    Completed ARRAYS Medium: Maximum Subarray

--- Q53.py
+++ Q53.py
@@ -1,13 +1,17 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        val_ = all(val < 0 for val in nums)
+        print(val_)
         if len(nums) == 1:
             return nums[0]
+        elif val_ is True:
+            return max(nums)
         else:
             # The max sum so far will be the sum of the entire array
             left = 0
             right = len(nums)
             max_sum = sum(nums)
-            while left < right:
+            while left <= right:
                 if nums[left] < nums[right-1]:
                     left = left + 1
                     max_sum = max(max_sum, sum(nums[left:right]))
@@ -16,6 +20,5 @@ class Solution:
                     max_sum = max(max_sum, sum(nums[left:right]))
                 else:
                     right = right - 1
-                    max_sum = max(max_sum, sum(nums[left:right]))
-
+                    
         return max_sum
