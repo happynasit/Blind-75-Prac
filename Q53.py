ef443f64285cda79efab043934552e6d1342d0f9
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-05 22:14:17 +0300

    Update Q53.py - Used the Kadane's ALGORITHM

--- Q53.py
+++ Q53.py
@@ -24,14 +24,11 @@ class Solution:
         # return max_sum
         maxSum = float('-inf')
         currentSum = 0
-        
+    
         for num in nums:
             currentSum += num
-            
             if currentSum > maxSum:
                 maxSum = currentSum
-            
             if currentSum < 0:
                 currentSum = 0
-        
         return maxSum
