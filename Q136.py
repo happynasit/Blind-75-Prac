22d7b6babc3e44a538bd5f6d0696536d4e1f9c6c
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-07 21:58:06 +0300

    Completed BINARY Easy: Single Number

--- Q136.py
+++ Q136.py
@@ -1,9 +1,6 @@
-def singleNumber(nums: List[int]) -> int:
-    """
-    Given a non-empty array of integers nums, every element appears twice 
-    except for one. Find that single one.
-    """
-    for i in nums:
-        count_n = nums.count(i) 
-        if count_n == 1:
-            return i
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        for number in set(nums):
+            if nums.count(number) == 1:
+                return number
