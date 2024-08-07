b71f1585efdceb4e155d9dc9e0c4df3f641f0d67
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-07 22:13:12 +0300

    Completed BINARY Easy: Single Number

--- Q136.py
+++ Q136.py
@@ -1,6 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        for number in set(nums):
-            if nums.count(number) == 1:
-                return number
+        dict_nums = dict()
+
+        for i in nums:
+            if i in dict_nums:
+                dict_nums[i] += 1
+            else:
+                dict_nums[i] = 1
+        
+        for i in dict_nums:
+            if dict_nums[i] == 1:
+                return i
