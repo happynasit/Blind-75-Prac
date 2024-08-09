b1ea3873caaf5bf88d4afa498f44ccd2c75f0e02
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-09 21:26:27 +0300

    Completed BINARY Medium: Single Number II

--- Q137.py
+++ Q137.py
@@ -1,13 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         nums_dict = dict()
-
         for i in nums:
             if i not in nums_dict:
                 nums_dict[i] = 1
             else:
                 nums_dict[i] += 1
-
         for i in nums_dict:
             if nums_dict[i] == 1:
                 return i
