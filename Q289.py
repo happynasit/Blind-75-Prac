78ff1478f650dbb130e6288aef2ab3670d9c4b4d
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-11 22:14:42 +0300

    Complete MATRIX medium: Game of Life

--- Q289.py
+++ Q289.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        
         for row in range(len(board)):
             for column in range(len(board[row])):
                 
