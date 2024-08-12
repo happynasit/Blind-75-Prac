815b64469311605ea9732d227e51470fe61c7c2f
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-12 15:54:26 +0300

    Completed MATH Easy: Sqrt(x)

--- Q69.py
+++ Q69.py
@@ -1,5 +1,6 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        
         if x==0:
             return 0
         elif x==1:
@@ -8,3 +9,21 @@ class Solution:
             for i in range(1,x+1):
                 if i*i > x:
                     return i-1
+        """
+        if x==0:
+            return 0
+        elif x==1:
+            return 1
+        else:
+            left = 0
+            right = x
+            while left <= right:
+                mid = (left + right) // 2
+                if mid * mid < x:
+                    left = mid + 1
+                elif mid * mid > x:
+                    right = mid - 1
+                else:
+                    return mid
+            return right
+        """
