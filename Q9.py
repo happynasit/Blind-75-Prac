97c8140a6ad82f7ccedc2130b45e1624d3040f8c
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-12 10:55:14 +0300

    Completed MATH Easy: Palindrome Number

--- Q9.py
+++ Q9.py
@@ -1,4 +1,17 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return True if str(x) == str(x)[::-1] else False
-    
+
+        # second method
+        # This runs slower, but still just math related so 
+        '''
+        if x < 0:
+            return False
+        else:
+            reverse = 0
+            xcopy = x
+            while x > 0:
+                reverse = (reverse * 10) + (x % 10)
+                x = x // 10
+            return reverse == xcopy
+        '''
