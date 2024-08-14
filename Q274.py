677b74882af846191b181e512b5808e44342ffd8
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-14 16:26:40 +0300

    Completed ARRAYS Medium: H-Index

--- Q274.py
+++ Q274.py
@@ -1,16 +1,8 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
+   
         citations.sort()
-
-        print(citations)
-
-        for i in range(len(citations)):
-            count = 1
-            for j in range(i, len(citations)):
-                count += 1
-                
-                if citations[i] == count:
-                    return citations[i]
-                    break
-                
-        return 1
+        for idx in range(len(citations)):
+            if len(citations) - idx <= citations[idx]:
+                return len(citations) - idx
+        return 0
