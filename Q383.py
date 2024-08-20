ad916d3b8e8bdca326d6ff38a8566da3bb16dcc2
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-20 16:38:44 +0300

    Completed HASHMAP Easy: Ransom Note

--- Q383.py
+++ Q383.py
@@ -15,6 +15,7 @@ class Solution:
         #         so_far = False
         # return so_far
 
+        
         st1, st2 = Counter(ransomNote), Counter(magazine)
         if st1 & st2 == st1:
             return True
