03bd1eeaa9570418a5f73d149ad8e9d4c3c69e9e
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-24 21:42:28 +0300

    Completed EASY HashMap: Isomorphic Strings

---
+++ Q205.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
+        s_dict = {}
+        t_dict = {}
+
+        for i in range(len(s)):
+            if s[i] in s_dict:
+                s_dict[s[i]].append(i)
+            else:
+                s_dict[s[i]] = [i]
+        
+        for j in range(len(t)):
+            if t[j] in t_dict:
+                t_dict[t[j]].append(j)
+            else:
+                t_dict[t[j]] = [j]
+        for locations in s_dict.values():
+            if locations not in t_dict.values():
+                return False
+        return True
+            
