6ba0879de50018b8008fb9c9920300a4c96cbcd2
happynasit <105992603+happynasit@users.noreply.github.com>
2024-08-29 22:34:36 +0300

    Completed SLIDING WINDOWS Medium: Minimum Size Subarray Sum

--- Q209.py
+++ Q209.py
@@ -1,21 +1,18 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         min_len = float('inf')
-
-        for index in range(len(nums)):
-            length = []            
-            j = index
-
-            while j < len(nums) and sum(length) < target:
-                length.append(nums[j])
-                j = j + 1
-            
-            print(length)
-            if sum(length) >= target:
-                min_len = min(min_len, len(length))
+        curr = 0 
+        left = 0
+        for right in range(len(nums)):
+            curr +=  nums[right]
+            while curr >= target:
+                min_len = min(min_len, right - left + 1)
+                curr -= nums[left]
+                left += 1
 
         if min_len != float('inf'):
             return min_len
         else:
             return 0
+
 
